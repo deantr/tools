1f73c002dab6c6d5c05117ab626f0ec93bf7b7f5
= <=>
2016-02-28 22:06:46 +0000

    added ignore

---
+++ cygwin/bash-aliases.sh
@@ -0,0 +1,18 @@
+alias a='alias'
+
+# shell
+
+a l='ls -lrt'
+a ls='ls -C'
+a la='la -a'
+a ll='ls -l'
+
+# git
+
+a gs='git status'
+a gc='git checkout'
+a gb='git branch'
+a gbr='git branch --remote'
+a ga='git add'
+
+
